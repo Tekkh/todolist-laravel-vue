539f5551a03d50a30f7a6423f0d2a328f8f0a2e6
tekkh <tkhorbi@gmail.com>
2022-02-16 16:23:15 +0100

    Add files

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 id="title">Todo List</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+   
+}
+</script>
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('/items', [ItemContronller::class, 'index']);
+Route::prefix('/item')->group( function (){
+    Route::post('/store', [ItemContronller::class, 'store']);
+    Route::put('/{id}', [ItemContronller::class, 'update']);
+    Route::delete('/{id}', [ItemContronller::class, 'destroy']);
+});
