a5236ad795c627f7d9074be84f365213f06cc2df
Tawfik KHORB <tkhorbi@gmail.com>
2022-02-18 11:48:40 +0100

    updated

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -42,3 +42,16 @@ export default {
    }
 }
 </script>
+<style scoped>
+.todoListContainer{
+    width: 350px;
+    margin: auto;
+}
+.heading{
+    background: #e6e6e6;
+    padding: 10px;
+}
+#title{
+    text-align: center;
+}
+</style>
